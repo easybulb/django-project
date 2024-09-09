639709d8f2405a105d7a9fa80807f0fbe547503f
Henry Ofodieze <easybulb@gmail.com>
2024-09-09 14:02:37 +0000

    Test django views.py

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello_world! Henry")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
